adde651151b0e181c808966f237544b85015bd05
huzaifasheikh2002 <huzaifanadeem621@gmail.com>
2022-04-21 02:30:10 +0500

    java assignment 4

---
+++ app.js
@@ -0,0 +1,99 @@
+
+        // Assignment # 26-30
+            // JAVASCRIPT
+
+            // MATH METHODS 
+
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// var number=prompt("Enter Positive Integer Number")
+//  var round=Math.round(number);
+// var floor=Math.floor(number);
+// var ceil=Math.ceil(number);
+// document.write(`Number: ${number} <br> 
+//                 Round off value: ${round} <br>
+//                 Floor value: ${floor} <br>
+//                 Ceil value: ${ceil}`)
+    ///////////////////////////////////////////////
+
+        // 2. Write a program that takes a negative floating point
+        // number from user & display the following in your browser.
+        // a. number
+        // b. round off value of the number
+        // c. floor value of the number
+        // d. ceil value of the number     
+
+// var number=prompt("Enter Negative Integer Number")
+//  var round=Math.round(number);
+// var floor=Math.floor(number);
+// var ceil=Math.ceil(number);
+// document.write(`Number: ${number} <br> 
+//                 Round off value: ${round} <br>
+//                 Floor value: ${floor} <br>
+//                 Ceil value: ${ceil}`)
+
+        ///////////////////////////////////////////
+
+// 3. Write a program that displays the absolute value of a
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+// var num =-5;
+// var abs=Math.abs(num)
+// document.write(`the absoulte value of ${num}is${abs}`)
+
+ ////////////////////////////////////////////////////////////
+
+
+            // 4. Write a program that simulates a dice using random()
+            // method of JS Math class. Display the value of dice in your
+            // browser.:
+//             var dice = Math.random() * 100 + 1
+// var dicefloor = Math.floor(dice)
+// document.write(`Random dice is : ${dicefloor}`)
+
+///////////////////////////////////////////////////////////////////////////////
+// 5. Write a program that simulates a coin toss using random()
+// method of JS Math class. Display the value of coin in your
+// browser
+
+// var coin= Math.random() * 2+1;
+// coin=Math.floor(coin)
+// console.log(coin);
+// if(coin===2){
+//     document.write(`${coin}</br> random coin value: heads`)
+// }
+// else if(coin===1){
+//     document.write(`${coin}</br> random coin value Tails`)
+// }
+////////////////////////////////////////////////////////////////////////////////
+// /Question No 06
+
+// var number = Math.random() * 100 + 1;
+// number = Math.floor(number);
+// document.write(`Random number b/w 1 to 100: ${number}`)
+
+
+// //Question No 07
+
+// var user = (prompt("Enter your weight in kilograms"));
+// document.write(`The weight of user is ${user} kilograms`)
+
+
+// //Question No 08
+
+// var user = parseInt(prompt("Enter Secret Number:"));
+// var randomNum = Math.random() * 10 + 1;
+// randomNum = Math.floor(randomNum)
+// console.log(randomNum)
+// if (user === randomNum) {
+//     alert("COngrats YOU WIN")
+// }
+// else {
+//     alert("Next time")
+// }
